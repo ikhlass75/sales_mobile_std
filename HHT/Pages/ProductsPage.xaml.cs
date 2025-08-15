9fc4fe1f59e4bcd45dfeed6aa966941319778cc1
ikhlass75 <ikhlassryahi@gmail.com>
2025-08-15 19:50:50 +0000

    Ajout de nouvelles pages à l'application

--- HHT/Pages/ProductsPage.xaml.cs
+++ HHT/Pages/ProductsPage.xaml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Maui.Controls;
+
 namespace HHT.Pages
 {
     public partial class ProductsPage : ContentPage
     {
+        public Command GoBackCommand { get; }
+
         public ProductsPage()
         {
             InitializeComponent();
+
+            GoBackCommand = new Command(async () =>
+            {
+                await Navigation.PopAsync();
+            });
+
+            BindingContext = this;
+        }
+
+        private async void HygieneTapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HygienePage());
         }
     }
 }
+
